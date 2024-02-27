12f9bb7cca78f52db3f672364313c96d34a09936
sly <shenliyan@sanss.com>
2024-02-27 14:29:14 +0800

    2-7  路由router初步定义

---
+++ router/router.go
@@ -0,0 +1,30 @@
+// 访问接口路由配置
+package router
+
+import (
+	"admin-go-ap/common/config"
+	"admin-go-api/middleware"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 初始化路由
+func InitRouter() *gin.Engine {
+	router := gin.New()
+	// 宕机恢复
+	router.Use(gin.Recovery())
+	// 跨域中间件
+	router.Use(middleware.Cors())
+	// 图片访问路径静态文件夹可直接访问
+	router.StaticFS(config.Config.ImageSettings.UploadDir, http.Dir(config.Config.ImageSettings.UploadDir))
+	// 日志log中间件
+	router.Use(middleware.Logger())
+	register(router)
+	return router
+}
+
+// 路由注册
+func register(router *gin.Engine) {
+	// ToDo 后续接口url
+}
